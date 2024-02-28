9f87d20a1347a8e064754fda9452dff55f64b460
Sena2K <senamurilo2@gmail.com>
2024-02-28 08:35:54 -0300

    Att

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-import numpy as np
+from problemas.problema import Problema
+from algoritmos.a_estrela import a_estrela
+from problemas.bracoRobotico import bracoRobotico
+
+
+
+if __name__ == "__main__":
+    problema = bracoRobotico()
+
+    (qtd_estados_visitados, no_solucao) = a_estrela(problema)
+
+    if (no_solucao is None):
+        print("Não houve solução ao problema")
+    else:
+        # caminho = no_caminho(no_solucao)
+        caminho = vertice_caminho(no_solucao)
+        print("Solução:")
+        print(caminho)
+
+    print(f"Estados visitados: {qtd_estados_visitados}")
+    print("Estado Inicial:")
+    print(problema.imprimir(problema.no_raiz))
+
+    custo = no_solucao.custoTotal()
+
+    print(custo)
---
+++ problemas/caixas.py
@@ -0,0 +1,3 @@
+class caixas:
+    def __init__(self, peso):
+        self.peso = peso
