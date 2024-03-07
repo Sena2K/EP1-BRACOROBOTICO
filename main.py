6a448f68e438092852cb19dec215a6ae231d7c24
Sena2K <senamurilo2@gmail.com>
2024-03-07 10:15:33 -0300

    att

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from algoritmos.bfs import bfs
 from algoritmos.a_estrela import a_estrela
 from algoritmos.dijkstra import dijkstra
 from auxiliar.Visitados import vertice_caminho, no_caminho
+
 from problemas.bracoRobotico import Braco
 
 if __name__ == "__main__":
     problema = Braco()
 
-    (qtd_estados_visitados, no_solucao) = dfs(problema)
-    # (qtd_estados_visitados, no_solucao) = bfs(problema)
-    # (qtd_estados_visitados, no_solucao) = a_estrela(problema)
-    # (qtd_estados_visitados, no_solucao) = dijkstra(problema)
+
+    (qtd_estados_visitados, no_solucao) = dijkstra(problema)
 
     if (no_solucao is None):
         print("Não houve solução ao problema")
@@ -22,5 +21,7 @@ if __name__ == "__main__":
         print(caminho)
 
     print(f"Estados visitados: {qtd_estados_visitados}")
-    print("Estado final:")
+    print("Estado Inicial:")
     print(problema.imprimir(problema.no_raiz))
+    print(problema.imprimir(no_solucao))
+    print(f"Custo: {no_solucao.custoTotal()}")
--- problemas/bracoRobotico.py
+++ problemas/bracoRobotico.py
@@ -6,53 +6,26 @@ from problemas.problema import Problema
 
 class Braco(Problema):
     def __init__(self):
-        self.estado_objetivo = np.array(["30", "20", "10", "R", ".", ".", ".", "|",
-                                         ".", ".", ".", "|", ".", ".", ".", "|",
-                                         ".", ".", ".", "|", ".", ".", ".", "|",
-                                         "60", "30", "20", "5", "R", ".", ".", "|",
-                                         ".", ".", ".", "|", ".", ".", ".", "|",
-                                         ".", ".", ".", "|", ".", ".", ".", "|",
+
+
+        self.estado_objetivo = np.array(["30", "20", "10", "R",
+                                         ".", ".", ".", "|",
+                                         ".", ".", ".", "|",
+                                         "30", ".", ".", "|",
+                                         ".", ".", ".", "|",
+                                         ".", ".", ".", "|",
                                          "."])
-        self.estado_inicial = np.array([".", ".", ".", "R", ".", ".", ".", "|",
-                                        "30", "10", ".", "|", "5", ".", ".", "|",
-                                        "20", "20", ".", "|", ".", ".", ".", "|",
-                                        "10", "60", ".", "R", ".", ".", ".", "|",
-                                        ".", ".", ".", "|", ".", ".", ".", "|",
-                                        ".", ".", ".", "|", ".", ".", ".", "|",
+        self.estado_inicial = np.array([".", ".", ".", "|",
+                                        "30", ".", ".", "|",
+                                        "20", ".", ".", "|",
+                                        "10", ".", ".", "R",
+                                        "30", ".", ".", "|",
+                                        ".", ".", ".", "|",
                                         "."])
 
-        # Método para inicializar o estado final com as caixas empilhadas corretamente
-        self.estado_final = self.inicializar_estado_final()
-
-
-    def inicializar_estado_final(self):
-        estado_final = np.full((6, 8), ".", dtype=str)  # Inicializa o estado final com pontos
-
-        # Cada coluna é uma lista que será preenchida com as caixas
-        colunas = [[] for _ in range(8)]
-
-        # Preenche as caixas nas colunas de acordo com o estado objetivo
-        index = 0
-        for coluna in range(8):
-            for linha in range(6):
-                if self.estado_objetivo[index] != ".":
-                    colunas[coluna].append(self.estado_objetivo[index])
-                index += 1
-
-        # Verifica se alguma pilha tem mais de 4 caixas
-        for pilha in colunas:
-            if len(pilha) > 4:
-                raise ValueError("Erro: Uma pilha não pode ter mais de 4 caixas.")
-
-        # Empilha as caixas em cada coluna no estado final
-        for coluna, pilha in enumerate(colunas):
-            for linha, caixa in enumerate(pilha):
-                estado_final[linha][coluna] = caixa
-
-        return estado_final
 
     def iniciar(self):
-        self.no_raiz = No(self.estado_objetivo)
+        self.no_raiz = No(self.estado_inicial)
         return self.no_raiz
 
     # Função auxiliar para imprimir
@@ -61,110 +34,276 @@ class Braco(Problema):
         maquina = ""
 
         for i in range(6):
-            for j in range(8):
-                index = i * 8 + j
+            for j in range(4):
+                index = i * 4 + j
                 maquina += estado[index] + " "
             maquina += "\n"
 
         maquina += estado[-1]
         return maquina
 
+
     def testar_objetivo(self, no):
         return np.array_equal(no.estado, self.estado_objetivo)
-
-    def calcular_custo_energia(self, acao, posicao, no):
-        custo = 0
-        if acao in ["⬅️", "➡️"]:
-            distancia = abs(posicao - np.where(no.estado == "R")[0][0])  # Calcula a distância percorrida
-            if distancia == 1:
-                custo += 1  # Movimento de uma casa custa 1 de energia
-            elif distancia > 2:
-                custo += int(distancia * 0.75)  # Movimento de mais de 2 casas custa 75% do movimento
-                if no.estado[posicao] != ".":  # Verifica se há uma caixa na posição atual
-                    peso_caixa = int(no.estado[posicao])  # Peso da caixa na posição atual
-                    custo += int(peso_caixa / 10)  # Cada 10kg aumenta o custo em 1 energia
-        elif acao == "Segurou":
-            if no.estado[posicao] != ".":  # Verifica se há uma caixa na posição atual
-                peso_caixa = int(no.estado[posicao])  # Peso da caixa na posição atual
-                custo += int(peso_caixa / 10)  # Cada 10kg aumenta o custo em 1 energia
-        return custo
 
     def gerar_sucessores(self, no):
         estado = no.estado
         nos_sucessores = []
 
-        # Encontra a posição do R (Braço)
+
         posicao = np.where(estado == "R")[0][0]
 
         expansoes = [self._direita, self._esquerda, self._agarrar, self._soltar]
         random.shuffle(expansoes)
         for expansao in expansoes:
             no_sucessor = expansao(posicao, no)
-            if no_sucessor is not None:
-                nos_sucessores.append(no_sucessor)
+            if no_sucessor is not None: nos_sucessores.append(no_sucessor)
 
         return nos_sucessores
 
     def _esquerda(self, posicao, no):
-        # Movimento para esquerda fazendo swap apenas na última coluna
-        if posicao % 4 != 0 and no.estado[posicao - 1] == "|":
+        # movimento para esquerda fazendo swap apenas na ultima coluna
+        if posicao not in [0, 1, 2, 3] and no.estado[posicao - 4] == "|":
+
             sucessor = np.copy(no.estado)
-            sucessor[posicao] = sucessor[posicao - 1]
-            sucessor[posicao - 1] = "R"
+            sucessor[posicao] = sucessor[posicao - 4]
+            sucessor[posicao - 4] = "R"
             return No(sucessor, no, "⬅️")
         else:
-            return None
+            None
 
     def _direita(self, posicao, no):
-        # Movimento para direita fazendo swap apenas na última coluna
-        if posicao % 4 != 3 and no.estado[posicao + 1] == "|":
+        # movimento para direita fazendo swap apenas na ultima coluna
+        if posicao not in [21, 22, 23, 24] and no.estado[posicao + 4] == "|":
+
             sucessor = np.copy(no.estado)
-            sucessor[posicao] = sucessor[posicao + 1]
-            sucessor[posicao + 1] = "R"
+            sucessor[posicao] = sucessor[posicao + 4]
+            sucessor[posicao + 4] = "R"
             return No(sucessor, no, "➡️")
         else:
-            return None
+            None
+
+    def _mover_direita_2(self, posicao, no):
+        # Movimento para a direita movendo 2 blocos
+        if posicao not in [19, 23, 24] and no.estado[posicao + 8] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao + 8]
+            sucessor[posicao + 8] = "R"
+
+            posicao += 8  # Atualiza a posição após mover 2 blocos
+
+            sucessor[posicao] = sucessor[posicao + 4]
+            sucessor[posicao + 4] = "."
+
+            return No(sucessor, no, "➡️")
+        else:
+            None
+
+    def _mover_esquerda_2(self, posicao, no):
+        # Movimento para a esquerda movendo 2 blocos
+        if posicao not in [4, 0, 1] and no.estado[posicao - 8] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao - 8]
+            sucessor[posicao - 8] = "R"
+
+            posicao -= 8  # Atualiza a posição após mover 2 blocos
+
+            sucessor[posicao] = sucessor[posicao - 4]
+            sucessor[posicao - 4] = "."
+
+            return No(sucessor, no, "⬅️")
+        else:
+            None
+
+    def _mover_direita_3(self, posicao, no):
+        # Movimento para a direita movendo 3 blocos
+        if posicao not in [18, 19, 23, 24] and no.estado[posicao + 12] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao + 12]
+            sucessor[posicao + 12] = "R"
+
+            posicao += 12  # Atualiza a posição após mover 3 blocos
+
+            sucessor[posicao] = sucessor[posicao + 8]
+            sucessor[posicao + 8] = "."
+
+            posicao += 8  # Atualiza a posição após mover 3 blocos
+
+            sucessor[posicao] = sucessor[posicao + 4]
+            sucessor[posicao + 4] = "."
+
+            return No(sucessor, no, "➡️")
+        else:
+            None
+
+    def _mover_esquerda_3(self, posicao, no):
+        # Movimento para a esquerda movendo 3 blocos
+        if posicao not in [5, 6, 1, 2] and no.estado[posicao - 12] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao - 12]
+            sucessor[posicao - 12] = "R"
+
+            posicao -= 12  # Atualiza a posição após mover 3 blocos
+
+            sucessor[posicao] = sucessor[posicao - 8]
+            sucessor[posicao - 8] = "."
+
+            posicao -= 8  # Atualiza a posição após mover 3 blocos
+
+            sucessor[posicao] = sucessor[posicao - 4]
+            sucessor[posicao - 4] = "."
+
+            return No(sucessor, no, "⬅️")
+        else:
+            None
+
+    def _mover_direita_4(self, posicao, no):
+        # Movimento para a direita movendo 4 blocos
+        if posicao not in [17, 18, 22, 23, 24] and no.estado[posicao + 12] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao + 12]
+            sucessor[posicao + 12] = "R"
+
+            posicao += 12  # Atualiza a posição após mover 4 blocos
+
+            sucessor[posicao] = sucessor[posicao + 8]
+            sucessor[posicao + 8] = "."
+
+            posicao += 8  # Atualiza a posição após mover 4 blocos
+
+            sucessor[posicao] = sucessor[posicao + 4]
+            sucessor[posicao + 4] = "."
+
+            return No(sucessor, no, "➡️")
+        else:
+            None
+
+    def _mover_esquerda_4(self, posicao, no):
+        # Movimento para a esquerda movendo 4 blocos
+        if posicao not in [4, 5, 1, 2, 3] and no.estado[posicao - 12] == "|":
+
+            sucessor = np.copy(no.estado)
+            sucessor[posicao] = sucessor[posicao - 12]
+            sucessor[posicao - 12] = "R"
+
+            posicao -= 12  # Atualiza a posição após mover 4 blocos
+
+            sucessor[posicao] = sucessor[posicao - 8]
+            sucessor[posicao - 8] = "."
+
+            posicao -= 8  # Atualiza a posição após mover 4 blocos
+
+            sucessor[posicao] = sucessor[posicao - 4]
+            sucessor[posicao - 4] = "."
+
+            return No(sucessor, no, "⬅️")
+        else:
+            None
 
     def _agarrar(self, posicao, no):
-        # Verifica se há caixas para pegar
-        if no.estado[posicao - 1] != ".":
-            # Pega a caixa com base no peso
-            caixa_superior = None
-            caixa_inferior = no.estado[posicao - 1]
 
-            # Verifica se há uma caixa inferior na pilha
-            if no.estado[posicao - 2] != ".":
-                caixa_superior = no.estado[posicao - 2]
-
-            # Verifica se há uma caixa no meio da pilha
-            if no.estado[posicao - 3] != ".":
-                caixa_do_meio = no.estado[posicao - 3]
-
-            # Compara os pesos das caixas para determinar qual pegar
-            if caixa_superior is not None and caixa_superior > caixa_inferior:
-                caixa_superior, caixa_inferior = caixa_inferior, caixa_superior
-            if caixa_do_meio is not None and caixa_do_meio > caixa_inferior:
-                caixa_do_meio, caixa_inferior = caixa_inferior, caixa_do_meio
-
-            # Atualiza o estado para refletir a caixa sendo agarrada
+        if no.estado[posicao - 3] != "." and no.estado[posicao - 2] != ".":
             sucessor = np.copy(no.estado)
-            sucessor[-1] = caixa_inferior
+            sucessor[-1] = no.estado[posicao - 1]
             sucessor[posicao - 1] = "."
             return No(sucessor, no, "Segurou")
+
+        elif no.estado[posicao - 2] != ".":
+            sucessor = np.copy(no.estado)
+            sucessor[-1] = no.estado[posicao - 2]
+            sucessor[posicao - 2] = "."
+            return No(sucessor, no, "Segurou")
+
+        elif no.estado[posicao - 3] != ".":
+            sucessor = np.copy(no.estado)
+            sucessor[-1] = no.estado[posicao - 3]
+            sucessor[posicao - 3] = "."
+            return No(sucessor, no, "Segurou")
         else:
-            return None
+            None
+
 
     def _soltar(self, posicao, no):
-        # Verifica se o braço está segurando uma caixa
-        if no.estado[-1] != ".":
-            # Determina a posição onde a caixa será solta
-            if no.estado[posicao - 1] != ".":
-                posicao_soltar = posicao - 1
-            elif no.estado[posicao - 2] != ".":
-                posicao_soltar = posicao - 2
-            elif no.estado[posicao - 3] != ".":
-                posicao_soltar = posicao - 3
-            else:
-                posicao_soltar = posicao - 3
 
-            # Atualiza o estado para
+        if no.estado[posicao - 3] != "." and no.estado[posicao - 2] != ".":
+            sucessor = np.copy(no.estado)
+            sucessor[posicao - 1] = no.estado[-1]
+            sucessor[-1] = "."
+            return No(sucessor, no, "Soltou")
+
+        elif no.estado[posicao - 3] != ".":
+            sucessor = np.copy(no.estado)
+            sucessor[posicao - 2] = no.estado[-1]
+            sucessor[-1] = "."
+            return No(sucessor, no, "Soltou")
+
+        elif no.estado[posicao - 3] == ".":
+            sucessor = np.copy(no.estado)
+            sucessor[posicao - 3] = no.estado[-1]
+            sucessor[-1] = "."
+            return No(sucessor, no, "Soltou")
+        else:
+            None
+
+    def custo(self, no, no_sucessor):
+        valor_custo = 1
+        estadoAtual = np.where(no.estado == "R")[0][0]
+        estadoFuturo = np.where(no_sucessor.estado == "R")[0][0]
+
+        # print(estadoAtual, estadoFuturo, estadoFuturo - estadoAtual)
+
+        # ato de pegar uma caixa n tem custo
+        if estadoFuturo - estadoAtual == 0:
+            return 0
+
+        # tem q arrumar -> np.where retorna os indices, algo usando mod talvez resolva
+        # o braco so anda um por vez ainda entao talvez seja isso tbm
+        # if(estadoFuturo - estadoAtual > 4)
+
+        # esse if deve resolver caso o braco ande mais de um espaco
+        if (estadoFuturo - estadoAtual) / 4 > 1:
+            valor_custo = (estadoFuturo - estadoAtual) * 0.75
+
+            # verifica se ta segurando uma caixa, se nao, custo 1
+        # se sim calcula o custo baseado no peso da caixa
+        if no.estado[-1] == ".":
+            return valor_custo
+        else:
+            valor_custo += int(no.estado[-1]) / 10
+            return valor_custo
+
+    def heuristica(self, no):
+        estado = no.estado
+        resultado = [
+            ["30", "20", "10", "R"],
+            [".", ".", ".", "|"],
+            [".", ".", ".", "|"],
+            ["30", ".", ".", "|"],
+            [".", ".", ".", "|"],
+            [".", ".", ".", "|"],
+            ["."]
+        ]
+        estado_matriz = [estado[0:4], estado[4:8], estado[8:12], estado[12:16],
+                         estado[16:20], estado[20:24]]
+
+        soma = 0
+
+        for i in range(len(resultado)):
+            for j in range(len(resultado[i])):
+                valor = resultado[i][j]
+                soma = soma + self._distancia_manhattan(valor, estado_matriz, i, j)
+
+        return soma
+
+    # Distância de Manhattan: d = |xi-xj| + |yi-yj|
+    def _distancia_manhattan(self, valor, estado, i, j):
+        for k in range(len(estado)):
+            for h in range(len(estado[k])):
+                if valor == estado[k][h]: return abs(i - k) + abs(j - h)
+        return 0
+
